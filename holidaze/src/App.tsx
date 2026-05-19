b74c1da4b9e8ae6bebbf94bc7cf287391bcff2b1
tin-kri <113044805+tin-kri@users.noreply.github.com>
2026-05-19 13:26:06 +0200

    FIX : source issues

--- holidaze/src/App.tsx
+++ holidaze/src/App.tsx
@@ -1,12 +1,20 @@
 import "./App.css";
-
+import { Calendar } from "./components/ui/calendar"
+import React from "react";
 
 function App() {
+const [date, setDate] = React.useState<Date | undefined>(new Date())
 
   return (
     <>
       <h1>Holidaze</h1>
      
+       <Calendar
+    mode="single"
+    selected={date}
+    onSelect={setDate}
+    className="rounded-lg border"
+  />
 
       <div className="grid grid-cols-3 gap-4">
         <button className="btn btn-primary w-full">Filter</button>
