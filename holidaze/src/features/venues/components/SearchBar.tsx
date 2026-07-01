7359a659eb56355bc92a10505c96b2b728c25a83
tin-kri <113044805+tin-kri@users.noreply.github.com>
2026-07-01 18:29:24 +0200

    WIP : Searchbar scaffolding

---
+++ holidaze/src/features/venues/components/SearchBar.tsx
@@ -0,0 +1,16 @@
+type SearchBarProps = {
+  
+};
+export default function SearchBar({  }: SearchBarProps)
+
+);
+// Input State
+// Request
+// Render result
+//    empty 
+//    loading 
+//    error
+//    success 
+
+needs to have
+ - debounce
