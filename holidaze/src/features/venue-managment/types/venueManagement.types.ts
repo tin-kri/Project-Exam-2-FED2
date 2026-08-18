3581901dd5f3eeb1d26b07613d3baa5c84d1c44f
tin-kri <113044805+tin-kri@users.noreply.github.com>
2026-08-18 14:27:31 +0200

    ADDS: zod schema and types for reciviing from API

---
+++ holidaze/src/features/venue-managment/types/venueManagement.types.ts
@@ -0,0 +1,5 @@
+import type { VenueApiData } from "@/features/venues/types/venue.types";
+import type { ApiResponse } from "@/types/types";
+ 
+
+export type CreateVenueResponse = ApiResponse<VenueApiData>; 
