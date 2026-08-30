cbe931171d4f9194347a316d1c407d12c33f4916
tin-kri <113044805+tin-kri@users.noreply.github.com>
2026-08-30 19:10:43 +0200

    adds layout form landingpage

--- holidaze/src/pages/LandingPage.tsx
+++ holidaze/src/pages/LandingPage.tsx
@@ -6,31 +6,45 @@ export default function LandingPage() {
   return (
     <>
       <div className="relative h-96 w-full  md:h-125">
+        
         <img
           src="https://images.unsplash.com/photo-1532915905335-03659fc83ff9?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Holidaze background cover image of a white villa with blue skies"
           className="h-full w-full object-center object-cover"
         />
 
-        <div className="absolute inset-0 bg-sky-200/20 mix-blend-darken" />
+        <div className="absolute inset-0 bg-sky-200/50 mix-blend-lighten" />
 
         <div className="absolute inset-0 flex items-center justify-center px-4 lg:px-24">
+          
           <div className="w-full max-w-3xl">
+            <div className="text-center mb-6">
+                         <span className="font-serif text-4xl font-bold tracking-tight text-primary"> Find your perfect venue <br/>
+</span>
+</div>
             <LandingSearch />
+            
           </div>
         </div>
       </div>
       <PageWrapper>
-        <h1 className="font-serif text-4xl font-bold tracking-tight text-primary mt-6">
-          Find your next stay
+        <div className="mt-12 border-t border-gray-200"></div>
+         <PopularVenues />
+        <div className="mt-12 border-t border-gray-200"></div>
+        <h1 className="font-serif text-center text-3xl font-bold tracking-tight text-primary mt-6">
+        Holidaze
           {/* <span className="font-serif text-3xl font-bold tracking-tight text-primary">
           Holidaze</span> */}
         </h1>
-        <p className=" font-sans text-xl text-foreground mt-3">
-          Discover holiday venues  handpicked for a unforgettable stay
-        </p>
-<PopularVenues />
+        <p className=" text-center font-sans text-xl text-foreground mt-3">
+Whether you're planning a weekend escape, a family holiday, or a dream 
+retreat — Holidaze connects you with unique venues for comfort, 
+character and unforgettable experiences. Browse hundreds of locations 
+across the world and book the stay you've always wanted.        </p>
+        <div className="mt-12 border-t border-gray-200"></div>
+       
         <Destinations />
+        <div className="mt-12 border-t border-gray-200"></div>
       </PageWrapper>
     </>
   );
