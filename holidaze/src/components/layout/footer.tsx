df1951c4e05d69ccfc2ff9adfe6f1ef67de9df69
tin-kri <113044805+tin-kri@users.noreply.github.com>
2026-09-11 15:44:20 +0200

    Wave fix: changes p in cards to h2

--- holidaze/src/components/layout/footer.tsx
+++ holidaze/src/components/layout/footer.tsx
@@ -11,9 +11,9 @@ export default function Footer() {
   return (
     <footer className="bg-primary">
       <div className="mx-auto flex w-full max-w-7xl flex-col items-start px-4 pt-16">
-        <p className="font-serif text-2xl font-bold tracking-tight text-background">
+        <h2 className="font-serif text-2xl font-bold tracking-tight text-background">
           Holidaze
-        </p>
+        </h2>
         <nav
           className="mt-6 flex flex-wrap gap-x-6 gap-y-3 text-xs"
           aria-label="Footer navigation"
--- holidaze/src/pages/landing-page/components/PopularVenueCard.tsx
+++ holidaze/src/pages/landing-page/components/PopularVenueCard.tsx
@@ -20,7 +20,7 @@ export default function PopularVenueCard({ popularVenue }: PopularVenueProps) {
         className=" h-50 w-full object-cover"
       />
       <div className="py-3 text-xl text-start text-primary">
-        <p className="font-serif font-bold ">{name}</p>
+        <h3 className="font-serif font-bold ">{name}</h3>
         <p className="text-sm text-accent-foreground">
           {location?.city}, {location?.country}
         </p>
